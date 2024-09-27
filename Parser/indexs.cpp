51624354a011b53c307f0c8b123d29d654d059b5
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2024-09-27 05:02:18 -0500

    Delete Parser/indexs.cpp

--- Parser/indexs.cpp
+++
@@ -1,7 +0,0 @@
-
-/*#include "../Estructura de datos/AVL/AVLfile.cpp"
-#include "../Estructura de datos/SequentialFile.cpp"
-#include "../Estructura de datos/Bplustree.cpp"
-#include "sstream"
-#include "Parser.cpp"
-#include <vector>*/
