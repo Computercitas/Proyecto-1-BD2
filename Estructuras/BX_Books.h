7a6cdd249e498603e7463f30ba6d428a09f1fed6
Davi Eler Magalhães <davi.eler.magalhaes@gmail.com>
2024-09-27 00:22:32 -0500

    class update

--- Estructuras/BX_Books.h
+++ Estructuras/BX_Books.h
@@ -1,11 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string.h>
-#include <fstream>
-#include <algorithm>
-#include <sstream>
 #include <string>
-#include <cmath>
 using namespace std;
 //ejemplo de data
 /*
@@ -13,12 +8,18 @@ ISBN;Book-Title;Book-Author;Year-Of-Publication;Publisher
 195153448;Classical Mythology;Mark P. O. Morford;2002;Oxford University Press
 2005018;Clara Callan;Richard Bruce Wright;2001;HarperFlamingo Canada
 */
+           
 /* ISBN (international standard book number) es un número identificador internacional
 para libros, hasta 2006 tenian hasta 10 digitos, pero hoy tienen 13 digitos. La base de datos
 tiene numeros de 10 digitos pero pondremos 13 caso se quiera expandirla futuramente con libros actuales*/
-
+/*
+Registro de mayor longitud en la columna 'Book-Title': 256 chars
+Registro de mayor longitud en la columna 'Book-Author': 143 chars
+Registro de mayor longitud en la columna 'Year-Of-Publication': 4 chars
+Registro de mayor longitud en la columna 'Publisher': 134 chars
+*/
 struct BX_Books {
-    char isbn[13];
+    unsigned long isbn;
     char book_title[256];
     char book_author[143];
     unsigned short year_of_publication;
@@ -26,7 +27,7 @@ struct BX_Books {
 
     BX_Books(){}
     BX_Books(string isbn, string book_title, string book_author, int year_of_publication, string publisher){
-        strncpy(this->isbn, isbn.c_str(), 13);
+        this->isbn = stoul(isbn);
         strncpy(this->book_title, book_title.c_str(), 256);
         strncpy(this->book_author, book_author.c_str(), 143);
         this->year_of_publication = year_of_publication;
@@ -34,9 +35,10 @@ struct BX_Books {
 
     void showData() const {
         cout << "ISBN: " << isbn << endl;
-        cout << "Book Title:" << book_title << endl;
+        cout << "Book Title: " << book_title << endl;
         cout << "Book Author: " << book_author << endl;
         cout << "Year of Publication: " << year_of_publication << endl;
         cout << "Publisher: " << publisher << endl;
+        cout << "----------------------------------" << endl;
     }
 };
