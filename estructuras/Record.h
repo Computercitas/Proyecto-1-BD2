21a76d5aa89f370cf29b6861181a53987b6d24e6
NoemiHuarino-utec <91573449+NoemiHuarino-utec@users.noreply.github.com>
2024-12-27 23:12:58 -0500

    record update

--- estructuras/Record.h
+++ estructuras/Record.h
@@ -1,65 +1,38 @@
-// Record.h
 #ifndef RECORD_H
 #define RECORD_H
 
+#include <iostream>
 #include <string>
-#include <vector>
+#include <sstream>
+#include <cstring>
+
 using namespace std;
 
-// Estructura para AdultDataset
-struct AdultRecord
+struct Record
 {
-    int dni;               // Documento Nacional de Identidad
-    string nombre;         // Nombre completo
-    string ocupacion;      // Ocupación
-    string estado_civil;   // Estado civil
-    float ingreso_mensual; // Ingreso mensual
-    int edad;              // Edad
-    char sexo;             // Sexo (M/F)
+  long dni;                // Documento Nacional de Identidad
+  char nombre[200];        // Nombre completo
+  char ocupacion[200];     // Ocupación
+  char estado_civil[200];  // Estado civil
+  long ingreso_mensual;    // Ingreso mensual
+  int edad;                // Edad
+  char sexo[2];            // Sexo (M/F)
+  bool is_deleted = false; // Indicador de registro eliminado
+  long next;               // Siguiente registro en la lista - hash
 
-    // Constructor por defecto
-    AdultRecord() {}
+  Record() = default;
 
-    // Constructor para inicializar un registro
-    AdultRecord(int dni, const string &nombre, const string &ocupacion, const string &estado_civil,
-                float ingreso_mensual, int edad, char sexo)
-        : dni(dni), nombre(nombre), ocupacion(ocupacion), estado_civil(estado_civil),
-          ingreso_mensual(ingreso_mensual), edad(edad), sexo(sexo) {}
-};
+  // Leer una línea del archivo CSV
+  void read(string fila);
 
-// Estructura para Superstore
-struct SuperstoreRecord
-{
-    int row_id;           // Identificador de fila
-    string order_id;      // Identificador de orden
-    string order_date;    // Fecha de orden
-    string ship_date;     // Fecha de envío
-    string ship_mode;     // Modo de envío
-    string customer_id;   // Identificador del cliente
-    string customer_name; // Nombre del cliente
-    string segment;       // Segmento de cliente
-    string city;          // Ciudad
-    string state;         // Estado
-    string product_id;    // Identificador del producto
-    string category;      // Categoría del producto
-    string sub_category;  // Subcategoría del producto
-    float sales;          // Ventas
-    int quantity;         // Cantidad vendida
-    float discount;       // Descuento
-    float profit;         // Ganancias
+  // Vaciar registro
+  void empty_record();
 
-    // Constructor por defecto
-    SuperstoreRecord() {}
+  // Imprimir datos del registro
+  void print() const;
 
-    // Constructor para inicializar un registro
-    SuperstoreRecord(int row_id, const string &order_id, const string &order_date, const string &ship_date,
-                     const string &ship_mode, const string &customer_id, const string &customer_name,
-                     const string &segment, const string &city, const string &state, const string &product_id,
-                     const string &category, const string &sub_category, float sales, int quantity, float discount, float profit)
-        : row_id(row_id), order_id(order_id), order_date(order_date), ship_date(ship_date),
-          ship_mode(ship_mode), customer_id(customer_id), customer_name(customer_name), segment(segment),
-          city(city), state(state), product_id(product_id), category(category), sub_category(sub_category),
-          sales(sales), quantity(quantity), discount(discount), profit(profit) {}
+  // Convertir a string
+  string toString() const;
 };
 
 #endif // RECORD_H
